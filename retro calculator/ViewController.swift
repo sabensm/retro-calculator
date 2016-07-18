d98cd8dbbbd759257c9fbd0203aa5e57097305da
Mike Sabens <michael.sabens@me.com>
2016-07-18 17:48:07 -0400

    First commit

--- retro calculator/ViewController.swift
+++ retro calculator/ViewController.swift
@@ -7,19 +7,130 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var buttonSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValString = ""
+    var rightValString = ""
+    var currentOperation: Operation = Operation.Empty
+    
+    var result = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Preparing Auido to play when button is pressed. We first define the name and type of file
+        //Then we define where it is and prepare the file to be played
+        //Finally, we do a try/catch to ensure it won't crash.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        do {
+            try buttonSound = AVAudioPlayer(contentsOfURL: soundURL)
+            buttonSound.prepareToPlay()
+        } catch let soundPlayError as NSError {
+            print(soundPlayError.debugDescription)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func clearButtonPressed(sender: AnyObject) {
+        
+        runningNumber = ""
+        leftValString = ""
+        rightValString = ""
+        
+        outputLabel.text = "0"
     }
 
+    @IBAction func numberPressed(btn: UIButton) {
+        playSound()
+        runningNumber += "\(btn.tag)"
+        outputLabel.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        
+        processOperation(Operation.Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        
+        processOperation(Operation.Multiply)
+    }
+    
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        
+        processOperation(Operation.Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
 
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(currentOperation)
+    }
+    
+    func processOperation(op: Operation) {
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            //Run Some Math!
+            
+            //A user selected an operator, but then selected another operater without first entering a number
+            if runningNumber != "" {
+                
+                rightValString = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValString)! * Double(rightValString)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValString)! / Double(rightValString)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValString)! - Double(rightValString)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValString)! + Double(rightValString)!)"
+                }
+                
+                leftValString = result
+                
+                outputLabel.text = result
+            }
+         
+            currentOperation = op
+            
+            
+        } else {
+            //This is the first time an operator has been pressed
+            leftValString = runningNumber
+            runningNumber = ""
+            currentOperation = op
+        }
+    }
+    
+    func playSound() {
+        if buttonSound.playing {
+            buttonSound.stop()
+        }
+        buttonSound.play()
+    }
+    
 }
 
